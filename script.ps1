50a349298a8d6d251b3c4ba2253b9a4481ced69a
Postiey (Tyler) <78533167+Postiey@users.noreply.github.com>
2022-11-12 03:04:39 -0600

    Added new features
    Cleaned script code up quite a bit along with adding python functionality. Went away with using if statements for when the user chooses a project type as I hope to expand this in the future to other project types, and a switch statement seems to allow for easier scalability.

--- script.ps1
+++ script.ps1
@@ -1,20 +1,49 @@
+#Change this to your own username on your computer
+$user_name = "posty"
+
+$file_path = "C:\Users\" + $user_name + "\Desktop\" + $project_name
+$extended_file_path = $file_path + $project_name
+
+$html_path = $extended_file_path + "\index.html"
+
+
 $project_type = Read-Host "Project Type: "
 $boilerplate = "C:\Users\posty\Desktop\boilerplate.txt"
 
+function CreateWebProject {
+    $project_name = Read-Host "Project Name: "
+    New-Item -Path C:\Users\$user_name\Desktop\Development\Web -Name $project_name -ItemType Directory
+    CreateWebProjectFiles
+    SetBoilerplate
+}
+
 function SetBoilerplate {
     $html_boilerplate = Get-Content -Path $boilerplate
-    $html_boilerplate | Out-File -FilePath C:\Users\posty\Desktop\$project_name\index.html
+    $html_boilerplate | Out-File -FilePath $html_path
 }
 
-function WebFileSystem {
-    New-Item -Path C:\Users\posty\Desktop\$project_name -Name index.html -ItemType File
-    New-Item -Path C:\Users\posty\Desktop\$project_name -Name styles.css -ItemType File
-    New-Item -Path C:\Users\posty\Desktop\$project_name -Name main.js -ItemType File
+function CreateWebProjectFiles {
+    New-Item -Path C:\Users\$user_name\Desktop\Development\Web\$project_name -Name index.html -ItemType File
+    New-Item -Path C:\Users\$user_name\Desktop\Development\Web\$project_name -Name styles.css -ItemType File
+    New-Item -Path C:\Users\$user_name\Desktop\Development\Web\$project_name -Name main.js -ItemType File
 }
 
-if ($project_type -eq "Web" -or $project_type -eq "web") {
+
+# Create python project
+function CreatePythonProject {
     $project_name = Read-Host "Project Name: "
-    New-Item -Path C:\Users\posty\Desktop\ -Name $project_name -ItemType Directory
-    WebFileSystem
-    SetBoilerplate
+    New-Item -Path C:\Users\$user_name\Desktop\Development\Python -Name $project_name -ItemType Directory
+    CreatePythonFiles
+}
+
+function CreatePythonFiles {
+    New-Item -Path C:\Users\$user_name\Desktop\Development\Python\$project_name -Name main.py -ItemType File
+}
+
+
+
+
+switch ($project_type) {
+    web {CreateWebProject; Break}
+    python {CreatePythonProject; Break}
 }
